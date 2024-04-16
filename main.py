278f9afb64ed4900b28cbc68d12f18ffc8ec3d54
龚建华 <58366505+GN-gjh@users.noreply.github.com>
2024-04-16 15:02:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,6 @@ def test1(model, path, datasets):
     model.load_state_dict(torch.load(path))
     model.eval()
 
-    # 存储相似度和对应索引的列表
     res = []
     count = 0
     for i in range(len(val_dataset)):
@@ -70,16 +69,10 @@ def test1(model, path, datasets):
         for j in range(len(train_dataset)):
             train_data_in, train_pearson, train_pcorr, train_label, train_pseudo, train_age, train_gender = train_dataset[j]
             vector2 = train_pearson.flatten()
-
             similarity = np.exp(-0.1*(np.linalg.norm(vector1.cpu().detach().numpy() - vector2.cpu().detach().numpy())))
-
-            # 存储相似度和对应的索引
             similarity_list.append((similarity, j))
-
-        # 按相似度降序排序
         sorted_similarity = sorted(similarity_list, key=lambda x: x[0], reverse=True)
 
-        # 选取前15个相似度最大的样本
         top_15_indices = [index for similarity, index in sorted_similarity[:15]]
         # print('top_15_indices:',top_15_indices)
         for j in top_15_indices:
@@ -92,7 +85,6 @@ def test1(model, path, datasets):
             ages.append(train_age)
             genders.append(train_gender)
 
-        # 获取对应的样本
         # top_15_samples = [self.train_dataset[index] for index in top_15_indices]    
         # print('top_15_samples:', len(top_15_samples))
 
@@ -122,7 +114,7 @@ def main(args):
         model = KMGCN(config, node_size, node_feature_size, timeseries_size, class_dim)
         model = model.to(device)
 
-        path = 'best_model/best_val_model_0.84375_66%.pth'
+        path = 'best_model_cn_ad%.pth'
         acc = test1(model, path, datasets)
         print('acc:', acc)
         
@@ -135,7 +127,6 @@ if __name__ == '__main__':
                         help='Configuration filename for training the model.')
     args = parser.parse_args()
     torch.cuda.set_device(0)
-    # 控制随机性
     seed = 0
     random.seed(seed)
     np.random.seed(seed)
